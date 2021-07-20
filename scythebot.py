f86ae943755b0e105e8af5d28f0402cffd593daa
Tadsz <13920647+Tadsz@users.noreply.github.com>
2021-07-20 23:36:41 +0200

    added message and reaction possibilities based on passing channel and message ids

--- scythebot.py
+++ scythebot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import json
 import random
 import discord
 from discord.ext import commands
@@ -11,6 +12,7 @@ from scythe.scythe import Scythe
 
 load_dotenv()
 dev_mode = True if os.getenv('SCYTHEBOT_DEBUG_MODE', False) == 'True' else False
+ADMINS = json.loads(os.getenv('ADMIN_DICT'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 VALHEIM_HOST = os.getenv('VALHEIM_HOST')
 VALHEIM_PORT = os.getenv('VALHEIM_PORT')
@@ -54,7 +56,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if len(message.content) > 25 or message.author.bot:
+    if len(message.content) > 100 or message.author.bot:
         return
     if any(x in message.content for x in thanks_notation):
         ctx = await bot.get_context(message)
@@ -99,5 +101,48 @@ async def valheim(ctx):
     await ctx.send(f'{server}:{VALHEIM_PORT}')
     return
 
+@bot.command(name='gpm')
+async def send_guild_message(ctx, channel_id, *args):
+    if ctx.author.id not in ADMINS.values():
+        return
+
+    if isinstance(int(channel_id), int):
+        channel_id = int(channel_id)
+        channel = bot.get_channel(channel_id)
+        message = ''
+        for word in args:
+            message += word + ' '
+        message = message[:-1]
+        await channel.send(message)
+    return
+
+@bot.command(name='rtm')
+async def react_to_message(ctx, channel_id, message_id, emoji):
+    if ctx.author.id not in ADMINS.values():
+        return
+
+    channel_id = int(channel_id)
+    message_id = int(message_id)
+    channel = await bot.fetch_channel(channel_id)
+    message = await channel.fetch_message(message_id)
+    await message.add_reaction(emoji)
+    return
+
+@bot.command(name='qtm')
+async def quote_to_message(ctx, channel_id, message_id, *args):
+    if ctx.author.id not in ADMINS.values():
+        return
+
+    channel_id = int(channel_id)
+    message_id = int(message_id)
+    channel = await bot.fetch_channel(channel_id)
+    message = await channel.fetch_message(message_id)
+
+    text_to_send = ''
+    for word in args:
+        text_to_send += word + ' '
+    text_to_send = text_to_send[:-1]
+    await message.reply(text_to_send)
+    return
 
 bot.run(TOKEN)
