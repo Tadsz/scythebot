c08db85b398df55206b208842f4d625bc141d0a2
Tadsz <13920647+Tadsz@users.noreply.github.com>
2022-02-03 00:15:47 +0100

    SB - allow multi fragment commands

--- soundboard/soundboard.py
+++ soundboard/soundboard.py
@@ -7,6 +7,7 @@ import os
 import glob
 import json
 import discord
+from asyncio import sleep
 from discord.ext import commands
 from dotenv import load_dotenv
 from discord import FFmpegPCMAudio, PCMVolumeTransformer
@@ -24,11 +25,16 @@ class SoundBoard(commands.Cog):
         # load default datasets
         sb_files = glob.glob('./soundboard/data/d1mp3/*.mp3')
         self.audio_dict = {i: f for i, f in enumerate(sb_files)}
-
+        self.still_talking = False
         return
 
     @commands.command(name='sbp')
-    async def play(self, ctx, audio_fragment: int):
+    async def play(self, ctx, *audio_fragment):
+
+        try:
+            audio_fragment = [int(i) for i in audio_fragment]
+        except:
+            return
 
         voice_channel = ctx.message.author.voice.channel
         voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
@@ -38,11 +44,22 @@ class SoundBoard(commands.Cog):
         else:
             await voice_client.move_to(voice_channel)
 
-        source = FFmpegPCMAudio(self.audio_dict.get(audio_fragment, './soundboard/data/d1mp3/aah.mp3'))
+        for frag in audio_fragment:
+            source = FFmpegPCMAudio(self.audio_dict.get(frag, './soundboard/data/d1mp3/aah.mp3'))
 
-        voice_client.play(source)
+            while self.still_talking:
+                # prevent playing the next fragment until the toggle is switched
+                await sleep(1)
+
+            self.still_talking = True
+            voice_client.play(source, after=self.talk_toggle)
 
         return
+
+    def talk_toggle(self, unknown_var):
+        self.still_talking = not self.still_talking
+        return
+
 
     @commands.command(name='sbl')
     async def leave_voice(self, ctx):
